81f0c21555ddc4f0bf6d43d9246d0a80a5dde7f5
M.Ozan Hazer <ozanhazer@gmail.com>
2020-03-27 19:51:11 +0300

    Menu: Selected category

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,6 +1,9 @@
-<ul class="nav flex-column">
+<ul class="nav nav-pills flex-column">
   <li class="nav-item">
-    <a class="nav-link active" href="#">All Threads</a>
+    <a class="nav-link {{Route::is('discuss.index') ? 'active' : ''}}"
+       href="{{route('discuss.index')}}">
+      All Threads
+    </a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">My Participation</a>
@@ -12,8 +15,11 @@
   <li class="dropdown-divider"></li>
 
   @foreach ($categories as $category)
-  <li class="nav-item">
-    <a class="nav-link" href="{{route('discuss.category', $category)}}">{{$category->name}}</a>
-  </li>
+    <li class="nav-item">
+      <a class="nav-link {{URL::current() == route('discuss.category', $category) ? 'active' : ''}}"
+         href="{{route('discuss.category', $category)}}">
+        {{$category->name}}
+      </a>
+    </li>
   @endforeach
 </ul>
